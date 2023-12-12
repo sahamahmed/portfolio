5add290d399552927d2852453bc5224ffbe3bb5d
sahamahmed <138794106+sahamahmed@users.noreply.github.com>
2023-12-12 22:51:08 +0500

    Update Aboutme.tsx

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -10,7 +10,7 @@ const Aboutme = () => {
 } />
       <div className='sm:text-lg tracking-wide flex flex-col gap-6'>
         <p>
-        HI there,I am a dedicated third-semester university student diving into the world of programming languages, currently honing skills in Next.js and Tailwind CSS. Aspiring to excel and contribute significantly to the tech industry.
+        I am a dedicated third-semester university student diving into the world of programming languages, currently honing skills in Next.js and Tailwind CSS. Aspiring to excel and contribute significantly to the tech industry.
         </p>
        
         <p>
